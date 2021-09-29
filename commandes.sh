98cdda69c6e01fab73799fa21398603bd9d6841f
Diego Devesa <77168416+TequiToa@users.noreply.github.com>
2021-09-29 03:20:39 +0200

    Update commandes.sh

--- commandes.sh
+++ commandes.sh
@@ -1,3 +1,9 @@
 sudo apt update && sudo apt upgrade -y
 
-apt install 
+apt install curl zsh kitty neofetch
+curl https://sh.rustup.rs -sSf | sh                                                     #Rust + Cargo
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin                    #kitty term
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"    #ohmyzsh
+
+cargo install 
